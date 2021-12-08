b4c2e39aa805c7962b6e7b3062241ed27602eefe
Bulichova <bulichova84@gmail.com>
2021-12-08 16:25:00 +0200

    dz

---
+++ index.js
@@ -0,0 +1,5 @@
+// import "./js/1.js"
+// import "./js/2.js"
+import './js/funExpres.js'
+// import "./js/4.js"
+// import "./js/5.js"
---
+++ js/funExpres.js
@@ -0,0 +1,6 @@
+const countSum = function (a, b) {
+  let result = a + b
+  return result
+}
+result = counrSum (2,4)
+console.log(result);
